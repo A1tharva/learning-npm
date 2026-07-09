29317212bd0b3624a190a6373f150b2e2d775c4d
Atharva Yadav <atharvayadav5472@gmail.com>
2026-07-10 04:36:17 +0530

    inital commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       .then(response => response.json())
+//       .then(json => console.log(json))
+
+import axios from 'axios'
+axios
+  .get('https://jsonplaceholder.typicode.com/todos/1')
+
+  .then((response) => {
+    console.log(response.data)
+  })
+
+  .catch((error) => {
+    console.log(error)
+  })
+
+// console.log('hello')
